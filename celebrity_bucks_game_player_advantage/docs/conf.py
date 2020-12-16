b423cf5dbf144263bc99ae3fe5208985f86ed5d9
Paul Platzman <pbp2113@columbia.edu>
2020-12-16 17:51:30 -0500

    Tinkering with documentation

--- celebrity_bucks_game_player_advantage/docs/conf.py
+++ celebrity_bucks_game_player_advantage/docs/conf.py
@@ -22,7 +22,6 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
 
-
 # -- Project information -----------------------------------------------------
 
 project = u'Celebrity Bucks Game Player Advantage'
